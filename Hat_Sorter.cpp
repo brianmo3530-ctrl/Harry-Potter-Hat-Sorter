d735459d26e982763458c889132d1bdf1ca25a05
Brian <brianmo3530@gmail.com>
2026-01-11 01:23:07 -0600

    Add Sorting Hat Quiz functionality
    Implement Sorting Hat Quiz logic with user input handling. part 1 of 3, will continue adding

---
+++ Hat_Sorter.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int gryffindor = 0;
+    int hufflepuff = 0;
+    int ravenclaw = 0;
+    int slytherin = 0;
+
+    int answer1 = 0;
+    int answer2 = 0;
+    int answer3 = 0;
+    int answer4 = 0;
+
+    std::cout << "The Sorting Hat Quiz!\n\n";
+
+    std::cout << "Q1) When I'm dead, I want people to remember me as:\n";
+    std::cout << "  1) The Good\n";
+    std::cout << "  2) The Great\n";
+    std::cout << "  3) The Wise\n";
+    std::cout << "  4) The Bold\n\n";
+    std::cin >> answer1;
+
+    if (answer1 == 1) {
+        hufflepuff++;
+    } else if (answer1 == 2) {
+        slytherin++;
+    } else if (answer1 == 3) {
+        ravenclaw++;
+    } else if (answer1 == 4) {
+        gryffindor++;
+    } else {
+        std::cout << "invalid input\n";
+    }
